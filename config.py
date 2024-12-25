da557b51d2a7c48428c7c75c0ac1a192eed75c39
Nxtx20 <testmod.king@gmail.com>
2024-12-25 17:39:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7974320647:AAHhmg_NxzWf4hdkFtawSQhbBiBPjo5Uhak"
+ADMIN_IDS = ["6496337480"]
+GROUP_ID = -  # Replace with your group ID
+GROUP_LINK = "https://t.me/VIKKI_VIP_DDOS"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
